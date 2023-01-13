9db291f658b01731db57b573c785be329d39dcef
Clairtonlm <clairtonlm@gmail.com>
2023-01-13 14:59:11 -0300

    Iniciando novo Projeto

---
+++ My_Project-02/POO/src/heranca/Carro.java
@@ -0,0 +1,22 @@
+package heranca;
+
+public class Carro {
+int velocidadeAtual;
+	
+	void acelerar() {
+		velocidadeAtual += 5;
+	}
+	
+	void frear() {
+		if(velocidadeAtual >= 5) {
+			velocidadeAtual -= 5;
+		}else {
+			velocidadeAtual = 0;
+		}
+		
+		}
+	
+	public String toString() {
+		return " Velocidade atual é :" + velocidadeAtual + "Km/h";
+	}
+}
---
+++ My_Project-02/POO/src/heranca/CarroTeste.java
@@ -0,0 +1,31 @@
+package heranca;
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+		Carro c1 = new Civic();
+		System.out.println("==========Civic=======================");
+		c1.acelerar();
+		System.out.println(c1);
+		
+		c1.acelerar();
+		System.out.println(c1);
+		
+		c1.acelerar();
+		System.out.println(c1);
+		
+		System.out.println("===========Ferrari====================");
+		
+		Ferrari c2 = new Ferrari();
+		
+		c2.acelerar();
+		System.out.println(c2);
+		
+		c2.acelerar();
+		System.out.println(c2);
+		c2.frear();
+		System.out.println(c2);
+
+	}
+
+}
